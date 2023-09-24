9c4bb7ae6139ca2a6ebfdc81a3f43fb8068d0e96
Baxtzodjon <rasulovbaxtzod@gmailcom>
2023-09-24 11:31:41 +0500

    3 homework figma arrays

---
+++ script.js
@@ -0,0 +1,42 @@
+// let arr = [[{ a: { price: 20 } }], [{ a: { price: 35 } }], [{ a: { price: 44 } }]];
+// let total = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         let price = arr[i][j].a.price;
+//         total += price;
+//     }
+// }
+
+// console.log(`Сумма цифр в массиве: ${total}`); 
+
+
+
+
+// do {
+//   number = prompt('Введите число больше 100:');
+// } while (number && number <= 100);
+
+// if (number) {
+//   alert('good');
+// } else {
+//   alert('Вы ввели некорректное число.');
+// }
+
+
+
+
+// let arr = [4, 16, 19, 22, 11, 144, 967, 19124];
+// let even = [];
+// let odd = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     even.push(arr[i]);
+//   } else {
+//     odd.push(arr[i]);
+//   }
+// }
+
+// alert(`Четные числа: ${even.join(', ')}`);
+// alert(`Нечетные числа: ${odd.join(', ')}`);
